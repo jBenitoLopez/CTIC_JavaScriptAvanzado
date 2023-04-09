d1376a3b66c4abda27fe3de3d8a2d87e6ca36b37
jBenitoLopez <lopez.jbenito@gmail.com>
2023-04-09 12:37:35 +0200

    mod: UD6 Jasmine  TodoListSpec.js implement TDD

--- Unidad06/jasmine-standalone-5.0.0-alpha.1/spec/TodoListSpec.js
+++ Unidad06/jasmine-standalone-5.0.0-alpha.1/spec/TodoListSpec.js
@@ -1,15 +1,40 @@
 describe("Una lista de tareas", function () {
-  it("debe estar vacía cuando está recién creada", function () {});
+  var todoList;
+
+  beforeEach(function () {
+    todoList = new TodoList();
+  });
+
+  it("debe estar vacía cuando está recién creada", function () {
+    expect(todoList.isEmpty()).toBe(true);
+  });
 
   describe("Cuando se añade una tarea", function () {
-    it("no debe estar vacía", function () {});
+    beforeEach(function () {
+      todoList.addTask("Explicar con un ejemplo");
+    });
 
-    it("debe poder contener más de una tarea", function () {});
+    it("no debe estar vacía", function () {
+      expect(todoList.isEmpty()).toBe(false);
+    });
 
-    it("debe estar sin completar nada más añadirse a la lista de tareas", function () {});
+    it("debe poder contener más de una tarea", function () {
+      todoList.addTask("Ir a la cena de navidad");
+      expect(todoList.size()).toBe(2);
+    });
 
-    it("se debe poder completar", function () {});
+    it("debe estar sin completar nada más añadirse a la lista de tareas", function () {
+      expect(todoList.task(0).isDone()).toBe(false);
+    });
 
-    it("debe quedar vacía al borrar la última tarea", function () {});
+    it("se debe poder completar", function () {
+      todoList.task(0).done();
+      expect(todoList.task(0).isDone()).toBe(true);
+    });
+
+    it("debe quedar vacía al borrar la última tarea", function () {
+      todoList.removeTask(0);
+      expect(todoList.isEmpty()).toBe(true);
+    });
   });
 });
