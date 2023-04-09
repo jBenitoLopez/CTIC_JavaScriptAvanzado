0baf16709e328e889c5b73e2285ee9aaef730e21
jBenitoLopez <lopez.jbenito@gmail.com>
2023-04-09 12:13:46 +0200

    mod: UD6 Jasmine  TodoListSpec.js

--- Unidad06/jasmine-standalone-5.0.0-alpha.1/spec/TodoListSpec.js
+++ Unidad06/jasmine-standalone-5.0.0-alpha.1/spec/TodoListSpec.js
@@ -3,9 +3,13 @@ describe("Una lista de tareas", function () {
 
   describe("Cuando se añade una tarea", function () {
     it("no debe estar vacía", function () {});
+
     it("debe poder contener más de una tarea", function () {});
+
     it("debe estar sin completar nada más añadirse a la lista de tareas", function () {});
+
     it("se debe poder completar", function () {});
+
     it("debe quedar vacía al borrar la última tarea", function () {});
   });
 });
