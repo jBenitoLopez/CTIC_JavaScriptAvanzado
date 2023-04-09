1c850ab7d0a17d7513f397cbbc248543b00c7504
jBenitoLopez <lopez.jbenito@gmail.com>
2023-04-09 12:10:54 +0200

    add: UD6 jastmine-standalone-tamplete

---
+++ Unidad06/jasmine-standalone-5.0.0-alpha.1/spec/TodoListSpec.js
@@ -0,0 +1,11 @@
+describe("Una lista de tareas", function () {
+  it("debe estar vacía cuando está recién creada", function () {});
+
+  describe("Cuando se añade una tarea", function () {
+    it("no debe estar vacía", function () {});
+    it("debe poder contener más de una tarea", function () {});
+    it("debe estar sin completar nada más añadirse a la lista de tareas", function () {});
+    it("se debe poder completar", function () {});
+    it("debe quedar vacía al borrar la última tarea", function () {});
+  });
+});
